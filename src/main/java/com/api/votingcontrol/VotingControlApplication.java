33a5486f8cec78090c44ad9e9d177e9007a48007
Miguel Honório <miguelshs@gmail.com>
2023-02-15 14:46:48 -0300

    Criação API

---
+++ src/main/java/com/api/votingcontrol/VotingControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.votingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/votingcontrol/VotingControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.votingcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
